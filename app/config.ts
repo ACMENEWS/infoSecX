d731a92beff81955f33a6c3e25923de21d2b70c9
Edwin Davidson <EdDavidson@ACMENEWS.com>
2019-11-02 16:23:17 -0500

    Removing private info from repo
    I revoked the exposed API keys and removed the config file from the repo

--- app/config.ts
+++ app/config.ts
@@ -4,11 +4,12 @@ export var ___xfeApiUrl = "https://api.xforce.ibmcloud.com/";
 export const _C = Object.freeze({
 	COMMA: ',' ,
 	SHODANSETTINGS: {
-					'shodanKey': 'PJNYYWH2ikdrSYlIMoTzz21JtdpdCNp4',
+					'shodanKeyOld': 'PJNYYWH2ikdrSYlIMoTzz21JtdpdCNp4',
+					'shodanKey': 'HUwooAVZJ4suNAMC3oEahmtyyAphwUBT',
 					 'attackSrc': [ '199.30.119.65', '199.30.119.99']
 				 },
 	XFESETTINGS: {
-					'apiusername': '520f5410-5eac-4150-9994-60dba4926d56',
-					'apipassword': 'b8427b24-019c-4a08-8ea4-3b2c844d8563'
+					'apiusername': 'f394059c-974a-4dda-b6b1-b5bc360688ab',
+					'apipassword': 'b85b2b6f-1056-42e1-946c-bd55d38f0671'
 	}
 });
